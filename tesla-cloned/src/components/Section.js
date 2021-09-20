3639aceeb92fde915d003f778d7e89d62fd157a2
Raaya <rahulagre555@gmail.com>
2021-09-20 12:50:02 +0530

    Built Section Component and styled it

--- tesla-cloned/src/components/Section.js
+++ tesla-cloned/src/components/Section.js
@@ -1,17 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
       <Buttons>
         <ButtonGroup>
-          <LeftButton>Custom Order</LeftButton>
-          <RightButton>Existing Inventory</RightButton>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
         </ButtonGroup>
         <DownArrow src="images/down-arrow.svg" />
       </Buttons>
@@ -30,7 +36,8 @@ const Wrap = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between; //vertical-alignment
-  align-items: center; //horizontal-alignment
+  align-items: center; //horizontal-alignment;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
 `;
 
 const ItemText = styled.div`
